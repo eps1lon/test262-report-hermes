2bf4fe8e63652eebfc053815cbd8c4f438a5a3cf
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2023-02-18 22:02:01 +0100

    Fix deploy (#1)
    f

--- app/browse/[...path]/page.js
+++ app/browse/[...path]/page.js
@@ -2,6 +2,13 @@ import * as React from "react";
 import Link from "next/link";
 import ReportSummary from "@/components/ReportSummary";
 
+export const dynamicParams = true;
+
+export async function generateStaticParams() {
+  // TODO: Why can't this be empty? https://github.com/vercel/next.js/issues/45850?
+  return [{ path: ["default"] }];
+}
+
 export default async function BrowsePage({ params }) {
   return (
     <React.Fragment>
