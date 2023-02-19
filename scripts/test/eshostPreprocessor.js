f48ce9acb7abca32ff3cc6daab7f49b279e646f4
eps1lon <silbermann.sebastian@gmail.com>
2023-02-19 19:57:00 +0100

    TODO: reword+fixup

--- scripts/test/eshostPreprocessor.js
+++ scripts/test/eshostPreprocessor.js
@@ -19,7 +19,15 @@ function shardPreprocessor(test, index) {
   }
 
   const targetShard = index % shardTotal;
-  debug(JSON.stringify({ targetShard, shardIndex, shardTotal, index }));
+  debug(
+    JSON.stringify({
+      targetShard,
+      relative: test.relative,
+      shardIndex,
+      shardTotal,
+      index,
+    })
+  );
   if (shardIndex === targetShard) {
     return test;
   } else {
