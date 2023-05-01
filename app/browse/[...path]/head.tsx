871b4797574bc4f7b9315d3b5031de9b6203c5c3
eps1lon <silbermann.sebastian@gmail.com>
2023-05-01 19:38:43 +0200

    TODO: reword+fixup

--- app/browse/[...path]/head.tsx
+++ app/browse/[...path]/head.tsx
@@ -1,5 +1,3 @@
-import type {} from "next";
-
 export default function Head({ params }: any) {
   // title needs a single string as children
   const title = `/${params.path.join("/")} Hermes test262 report`;
